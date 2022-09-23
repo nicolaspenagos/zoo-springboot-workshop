8f0c94fa2cf281cec4fcd3cd5e2c0942be1ec620
nicolaspenagos <nicolas.penagosm98@gmail.com>
2022-09-22 22:46:22 -0500

    Exception msg was modified

--- src/main/java/co/edu/icesi/zoo/constant/AnimalErrorMsgs.java
+++ src/main/java/co/edu/icesi/zoo/constant/AnimalErrorMsgs.java
@@ -5,7 +5,7 @@ public class AnimalErrorMsgs {
     public final static String IMPOSSIBLE_DATE_MSG = "Impossible date: the arrival date cannot be later than the current date";
     public final static String WRONG_DATE_FORMAT_MSG = "Wrong date format: text cannot be parsed to a date, make sure you are using ISO 8601 yyyy-mm-ddThh:mm:ss format";
     public final static String WRONG_NAME_FORMAT_MSG = "Wrong name format: name cannot exceed 120 digits length and can only contain letters and spaces";
-    public final static String WRONG_PYTHON_CHARACTERISTICS_MSG = String.format("Wrong specie characteristics: age must be between 0 and %1$S (years), height between 0 and %2$S (meters) and weight between 0 and %3$S (kg)", BurmesePython.MAX_AGE, BurmesePython.MAX_HEIGHT, BurmesePython.MAX_WEIGHT);
+    public final static String WRONG_PYTHON_CHARACTERISTICS_MSG = String.format("Wrong specie characteristics: age must be between 0 up to %1$S (years), height between 0 and %2$S (meters) and weight between 0 and %3$S (kg) (0 is always excluded)", BurmesePython.MAX_AGE, BurmesePython.MAX_HEIGHT, BurmesePython.MAX_WEIGHT);
     public final static String NOT_UNIQUE_NAME_MSG = "Not unique name: the name of the animal must be unique";
     public final static String WRONG_MOTHER_SEX = "Wrong mother sex: mother has to be female (sex = 'F')";
     public final static String WRONG_FATHER_SEX = "Wrong father sex: father has to be male (sex='M')";
